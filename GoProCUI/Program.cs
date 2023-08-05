d1b4c2104e4048eb4f6c7401d4f33a367908455f
NidoKota <36328961+NidoKota@users.noreply.github.com>
2023-08-05 20:06:04 +0900

    Initial commit

---
+++ GoProCUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace GoProCUI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+        }
+    }
+}
